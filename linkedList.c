95545a81b56dc014f4b247d9001a7f58a73eb960
Logan Miller <millelog@oregonstate.edu>
2016-10-17 13:54:18 -0700

    Initial commit

---
+++ linkedList.c
@@ -0,0 +1,54 @@
+#include "linkedList.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+
+struct Link* listInsertionSort(struct Link* head) {
+
+  /*
+   * This function should perform an insertion sort on the list whose head is
+   * provided as the function's argument, so that the values in the list are
+   * sorted in ascending order, starting at the head.
+   *
+   * The sort should be done without allocating any new Link structs or any
+   * other auxiliary data structures.
+   *
+   * Return a pointer to the new head of the list.
+   */
+  return head;
+
+}
+
+
+struct Link* reverseList(struct Link* head) {
+
+  /*
+   * This function should iteratively reverse the list whose head is provided
+   * as the function's argument.
+   *
+   * The reversal must be done totally in place, i.e. you may not allocate any
+   * new Link structs or any other auxiliary data structures.
+   *
+   * Return a pointer to the new head of the list.
+   */
+  return head;
+
+}
+
+
+struct Link* reverseListRecursive(struct Link* head){
+
+  /*
+   * Write this function for extra credit.  It should do the exact same thing
+   * as reverseList() above, except it should do it recursively instead of
+   * iteratively (i.e. no loops allowed).
+   *
+   * Again, you may not allocate any new Link structs or any other auxiliary
+   * data structures.  You may, however, define an auxiliary function to help
+   * you do the recursion.
+   *
+   * Return a pointer to the new head of the list.
+   */
+  return head;
+
+}
